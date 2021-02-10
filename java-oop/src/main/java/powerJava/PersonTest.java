af3bdc4652edc7d23821639b30b6709b17fa5ed9
bit306 <bit306@DESKTOP-UMBRTAG>
2021-02-10 23:50:25 +0900

    this. 실습

---
+++ java-oop/src/main/java/powerJava/PersonTest.java
@@ -0,0 +1,33 @@
+package powerJava;
+
+
+class Person{
+	private String lastName;
+	private String firstName;
+	
+	String getLastName() {
+		return lastName;
+	}
+	
+	String getFirstName() {
+		return firstName;
+	}
+	
+	public Person(String lastName, String firstName) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+	}
+	
+	public String buildName() {
+		return String.format("%s %s\n", this.getLastName(), this.getFirstName());
+	}
+	
+}
+
+
+public class PersonTest {
+	public static void main(String [] args) {
+		Person person = new Person("홍", "길동");
+		System.out.println(person.buildName());
+	}
+}
